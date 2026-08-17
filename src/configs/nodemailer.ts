73294da014e42cd672bf0f21ea4475aa07aa254a
zub-bee <zubbeed21.18@gmail.com>
2026-08-17 20:00:42 +0100

    dep: use nodemailer for email
    - in dev environment in absence of domain

---
+++ src/configs/nodemailer.ts
@@ -0,0 +1,12 @@
+import { logger } from "@/utils/logger";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD
+  }
+});
+
+export default transporter;
