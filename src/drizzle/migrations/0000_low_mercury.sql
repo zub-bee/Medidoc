d9482b149bf9a07c66a3be0d4cab5ac60b7828f1
zub-bee <zubbeed21.18@gmail.com>
2026-08-25 21:59:29 +0100

    refactor: update lint-staged configuration and modify user role schema
    - Removed TypeScript compilation check from lint-staged configuration.
    - Updated user role options in the user schema to include new roles: patient, provider, practitioner, and platform_admin.

---
+++ src/drizzle/migrations/0000_low_mercury.sql
@@ -0,0 +1,20 @@
+CREATE TABLE "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text,
+	"role" text NOT NULL,
+	"is_email_verified" boolean DEFAULT false NOT NULL,
+	"last_login_at" timestamp,
+	"failed_login_attempts" integer DEFAULT 0 NOT NULL,
+	"lock_until" timestamp,
+	"avatar" json,
+	"provider" text DEFAULT 'local' NOT NULL,
+	"provider_id" text,
+	"is_deleted" boolean DEFAULT false NOT NULL,
+	"deleted_at" timestamp,
+	"re_activate_available_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
