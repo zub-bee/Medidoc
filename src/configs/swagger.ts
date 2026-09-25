5dd24739d9765a59501c4fe935309ef2da872b8e
zub-bee <zubbeed21.18@gmail.com>
2026-09-25 23:54:57 +0100

    refactor: update swagger setup to dynamically read swagger.json file

--- src/configs/swagger.ts
+++ src/configs/swagger.ts
@@ -1,7 +1,14 @@
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import env from "../configs/env";
-import swaggerDocument from "../docs/swagger.json";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const swaggerDocument = JSON.parse(
+  readFileSync(join(__dirname, "../docs/swagger.json"), "utf-8")
+);
 
 export const setupSwagger = (app: Express) => {
   if (env.NODE_ENV !== "development") return;
